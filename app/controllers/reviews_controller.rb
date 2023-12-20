ab09b262e9846c0680dd4e2f1a213affbd36af0b
Severine Bader <severine.bader@gmail.com>
2023-12-20 17:18:10 +0100

    add reviews + show in restau + add restau btn

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,33 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant
+
+
+  def new
+    @review = @restaurant.review.new
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def create
+    @review = @restaurant.review.new(review_params)
+    if @review.save
+      redirect_to restaurants_path(@restaurant), notice: 'Review was successfully created.'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+
 end
