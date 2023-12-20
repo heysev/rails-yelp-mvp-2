3da8eb3ac7a84a8daeafcf30b7a79c1b842fd650
Severine Bader <severine.bader@gmail.com>
2023-12-20 14:56:45 +0100

    update validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :review, dependent: :destroy
+
+  validates :name, :address, :phone, :category, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
 end
